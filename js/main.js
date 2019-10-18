db20837be420411338c02bb17365ebd99f117bba
alex_molodoy <sivtsov_a@inbox.ru>
2019-10-18 22:40:04 +0300

    module3-task2v6

--- js/main.js
+++ js/main.js
@@ -1,73 +1,88 @@
 'use strict';
 
-var PinSizes = {WIDTH: 70, HEIGHT: 50, RADIUS: 25};
-var MapSets = {LEFT: 0, TOP: 130, RIGHT: 1200, BOTTOM: 630};
-var COUNT_ADV = 8;
-var MASS_ADV_TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var MASS_ADV_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var MASS_ADV_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var PinSize = {
+  WIDTH: 70,
+  HEIGHT: 50,
+  RADIUS: 25
+};
+
+var MapRect = {
+  LEFT: 0,
+  TOP: 130,
+  RIGHT: 1200,
+  BOTTOM: 630
+};
+
+var ADV_COUNT = 8;
+
+var ADV_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var ADV_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var ADV_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-var createAdvs = function () {
+var createAds = function (count) {
+  var ads = [];
 
-  var massOfferVar = [];
-  for (var i = 0; i < COUNT_ADV; i++) {
-    massOfferVar =
-    {
-      'author': {
-        'avatar': 'img/avatars/user0' + i + '.png'
+  for (var i = 1; i < count; i++) {
+    ads.push({
+      author: {
+        avatar: 'img/avatars/user0' + i + '.png'
       },
 
-      'offer': {
-        'title': 'заголовок предложения',
-        'address': 'string',
-        'price': 100500,
-        'type': MASS_ADV_TYPES[getRandomNumber(0, MASS_ADV_TYPES.length)],
-        'rooms': 4,
-        'guests': 4,
-        'checkin': 'string',
-        'checkout': 'string',
-        'features': MASS_ADV_FEATURES.slice(0, getRandomNumber(0, MASS_ADV_FEATURES.length)),
-        'description': 'string',
-        'photos': MASS_ADV_PHOTOS.slice(0, getRandomNumber(0, MASS_ADV_PHOTOS.length))
+      offer: {
+        title: 'заголовок предложения',
+        address: 'string',
+        price: 100500,
+        type: ADV_TYPES[getRandomNumber(0, ADV_TYPES.length)],
+        rooms: 4,
+        guests: 4,
+        checkin: 'string',
+        checkout: 'string',
+        features: ADV_FEATURES.slice(0, getRandomNumber(0, ADV_FEATURES.length)),
+        description: 'string',
+        photos: ADV_PHOTOS.slice(0, getRandomNumber(0, ADV_PHOTOS.length))
       },
 
-      'location': {
-        'x': getRandomNumber(MapSets.LEFT, MapSets.RIGHT),
-        'y': getRandomNumber(MapSets.TOP, MapSets.BOTTOM)
+      location: {
+        x: getRandomNumber(MapRect.LEFT, MapRect.RIGHT),
+        y: getRandomNumber(MapRect.TOP, MapRect.BOTTOM)
       }
-    };
+    });
   }
 
-  return massOfferVar;
+  return ads;
 };
 
+var map = document.querySelector('.map');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var pinContainer = document.querySelector('.map__pins');
 
-var createPin = function (inputElement) {
-  var mapPin = pinTemplate.cloneNode(true);
-  mapPin.style.left = (inputElement.location.x - PinSizes.RADIUS) + 'px';
-  mapPin.style.top = (inputElement.location.y - PinSizes.HEIGHT) + 'px';
+var createPin = function (ad) {
+  var pin = pinTemplate.cloneNode(true);
+  var pinImage = pin.querySelector('img');
 
-  var pinPicture = document.querySelector('.map__pin');
-  mapPin.appendChild(pinPicture);
+  pin.style.left = (ad.location.x - PinSize.RADIUS) + 'px';
+  pin.style.top = (ad.location.y - PinSize.HEIGHT) + 'px';
+  pinImage.src = ad.author.avatar;
+  pinImage.alt = ad.offer.type;
 
-  return mapPin;
+  return pin;
 };
 
-var pinElements = document.querySelector('#pin');
+var renderPins = function (ads) {
+  var fragment = document.createDocumentFragment();
+  ads.forEach(function (ad) {
+    fragment.appendChild(createPin(ad));
+  });
 
-var fragmentPin = document.createDocumentFragment();
+  pinContainer.appendChild(fragment);
+};
 
-createAdvs.forEach(function (createAdv) {
-  var buttonFragment = createPin(createAdv);
-  fragmentPin.appendChild(buttonFragment);
-});
+var ads = createAds(ADV_COUNT);
 
-pinElements.appendChild(fragmentPin);
+renderPins(ads);
 
-var delClass = document.querySelector('.map');
-delClass.classList.remove('map--faded');
+map.classList.remove('map--faded');
